e1130ce02d2bc655e711356e0a2ebaeee6b66ea0
KRJaeh0 <145985933+JJH0204@users.noreply.github.com>
2025-08-04 10:30:53 +0900

    Update G.O.S

--- Assets/Scripts/VisualScripting/Input/Trigger/TriggerExit.cs
+++ Assets/Scripts/VisualScripting/Input/Trigger/TriggerExit.cs
@@ -2,20 +2,35 @@ using UnityEngine;
 
 namespace _Project.Scripts.VisualScripting
 {
-public class TriggerExit : ProcessBase
-{
-    [SerializeField] private string selectedTag = "";
-
-    private void OnTriggerExit(Collider other)
+    public class TriggerExit : ProcessBase
     {
-        if (other.CompareTag(selectedTag))
-            Execute();
-    }
+        [SerializeField] private string selectedTag = "";
 
-    public override void Execute()
-    {
-        IsOn = true;
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag(selectedTag))
+                // Execute();
+                IsOn = true;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(selectedTag))
+                IsOn = false;
+        }
+
+        public override void Execute()
+        {
+            IsOn = true;
+        }
+        
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.blue;
+		        
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+        }
     }
-}
     
 }
--- Assets/Scripts/VisualScripting/Output/SetActivate/ToggleActivate.cs
+++ Assets/Scripts/VisualScripting/Output/SetActivate/ToggleActivate.cs
@@ -2,20 +2,20 @@ using UnityEngine;
 
 namespace _Project.Scripts.VisualScripting
 {
-public class ToggleActivate : ProcessBase
-{
-    [SerializeField] private GameObject obj;
-
-    private void Start()
+    public class ToggleActivate : ProcessBase
     {
-        IsOn = obj.activeSelf;
-    }
+        [SerializeField] private GameObject obj;
+        
+        // TODO: 
+        private void Start()
+        {
+            IsOn = obj.activeSelf;
+        }
 
-    public override void Execute()
-    {
-        IsOn = obj.activeSelf;
-        obj.SetActive(!IsOn);
+        public override void Execute()
+        {
+            IsOn = obj.activeSelf;
+            obj.SetActive(!IsOn);
+        }
     }
-}
-    
 }
